47a84e91d61386e79a6a8ef1b267cd7cd7f54a31
Juan David Segura <Juan David Segura@DESKTOP-IRI36F8>
2018-04-15 17:59:57 -0500

    Agregada clase

--- ComponentMap/src/main/java/com/jbadcode/util/ComponentMap.java
+++ ComponentMap/src/main/java/com/jbadcode/util/ComponentMap.java
@@ -8,7 +8,29 @@ package com.jbadcode.util;
 /**
  *
  * @author JBadCode
+ * @param <K>
+ * @param <V>
  */
-public class ComponentMap {
-    
+public class ComponentMap<K, V> {
+
+    K key;
+    V value;
+    ComponentMap<K, V> tree;
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public ComponentMap<K, V> getTree() {
+        return tree;
+    }
+
+    public void setTree(ComponentMap<K, V> tree) {
+        this.tree = tree;
+    }
+
 }
